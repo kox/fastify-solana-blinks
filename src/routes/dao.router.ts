cc6e10390864f99c480585eba7898357daa20af1
kox <garsanzi@gmail.com>
2024-08-01 17:58:38 +0200

    adding generug download and saving

--- src/routes/dao.router.ts
+++ src/routes/dao.router.ts
@@ -1,94 +1,110 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS, createPostResponse, MEMO_PROGRAM_ID } from '@solana/actions';
+import {
+  ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
+  MEMO_PROGRAM_ID,
+} from '@solana/actions';
 import { STANDARD } from '../constants';
-import { clusterApiUrl, ComputeBudgetProgram, Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
+import {
+  clusterApiUrl,
+  ComputeBudgetProgram,
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from '@solana/web3.js';
 /* import * as controllers from '../controllers'; */
 /* import { utils } from '../utils'; */
 /* import { loginSchema, signupSchema } from '../schemas/User'; */
 
 async function daoRouter(fastify: FastifyInstance) {
-    fastify.route({
-        method: ['GET', 'OPTIONS'],
-        url: '/',
-        schema: {},
-        handler: async (request: FastifyRequest, reply: FastifyReply) => {
-            console.log(request.hostname)
-            const payload: ActionGetResponse = {
-                icon: `${request.protocol}://${request.hostname}/public/solana_talent_olympics.png`,
-                label: "Talent Olympics Test",
-                description: "This is a test for the Talent Olympics demo",
-                title: "Talent Olympics",
-            }
-            return reply
-                .code(STANDARD.OK.statusCode)
-                .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
-                .send(payload);
+  fastify.route({
+    method: ['GET', 'OPTIONS'],
+    url: '/',
+    schema: {},
+    handler: async (request: FastifyRequest, reply: FastifyReply) => {
+      console.log(request.hostname);
+      const payload: ActionGetResponse = {
+        icon: `${request.protocol}://${request.hostname}/public/solana_talent_olympics.png`,
+        label: 'Talent Olympics Test',
+        description: 'This is a test for the Talent Olympics demo',
+        title: 'Talent Olympics',
+      };
+      return reply
+        .code(STANDARD.OK.statusCode)
+        .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
+        .send(payload);
+    },
+  });
+
+  fastify.post(
+    '/',
+    {},
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const body = request.body as ActionPostRequest;
+
+        let account: PublicKey;
+
+        try {
+          account = new PublicKey(body.account);
+        } catch (err) {
+          return reply
+            .code(STANDARD.ERROR.statusCode)
+            .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
+            .send({
+              errorMessage:
+                'Wrong account. Check again you send a body parameter a correct Publickey',
+            });
         }
-    });
 
-    fastify.post(
-        '/',
-        {},
-        async (request: FastifyRequest, reply: FastifyReply) => {
-            try {
-                const body = request.body as ActionPostRequest;
+        const transaction = new Transaction();
 
-                let account: PublicKey;
+        transaction.add(
+          ComputeBudgetProgram.setComputeUnitPrice({
+            microLamports: 1000,
+          }),
+          new TransactionInstruction({
+            programId: new PublicKey(MEMO_PROGRAM_ID),
+            data: Buffer.from('this is a simple memo message', 'utf8'),
+            keys: [],
+          }),
+        );
 
-                try {
-                    account = new PublicKey(body.account);
-                } catch (err) {
-                    return reply
-                    .code(STANDARD.ERROR.statusCode)
-                    .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
-                    .send({
-                        errorMessage: 'Wrong account. Check again you send a body parameter a correct Publickey'
-                    })
-                }
+        transaction.feePayer = account;
 
-                const transaction = new Transaction();
+        const connection = new Connection(clusterApiUrl('devnet'));
+        transaction.recentBlockhash = (
+          await connection.getLatestBlockhash()
+        ).blockhash;
 
-                transaction.add(
-                    ComputeBudgetProgram.setComputeUnitPrice({
-                        microLamports: 1000,
-                    }),
-                    new TransactionInstruction({
-                        programId: new PublicKey(MEMO_PROGRAM_ID),
-                        data: Buffer.from("this is a simple memo message", "utf8"),
-                        keys: []
-                    })
-                );
+        const payload: ActionPostResponse = await createPostResponse({
+          fields: {
+            transaction,
+            message: 'Thanks for signing the transaction',
+          },
+          // signers: []
+        });
 
-                transaction.feePayer = account;
+        return reply
+          .code(STANDARD.ACCEPTED.statusCode)
+          .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
+          .send(payload);
+      } catch (err) {
+        console.log(err);
 
-                const connection = new Connection(clusterApiUrl("devnet"));
-                transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-
-                const payload: ActionPostResponse = await createPostResponse({
-                    fields: {
-                        transaction,
-                        message: "Thanks for signing the transaction"
-                    },
-                    // signers: []
-                })
-
-                return reply
-                    .code(STANDARD.ACCEPTED.statusCode)
-                    .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
-                    .send(payload);
-
-            } catch(err) {
-                console.log(err);
-
-                return reply
-                    .code(STANDARD.ERROR.statusCode)
-                    .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
-                    .send({
-                        errorMessage: "Something went wrong"
-                    })
-            }
-        }
-    )
+        return reply
+          .code(STANDARD.ERROR.statusCode)
+          .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
+          .send({
+            errorMessage: 'Something went wrong',
+          });
+      }
+    },
+  );
 }
 
 export default daoRouter;
--- src/routes/downloadRug.router.ts
+++ src/routes/downloadRug.router.ts
@@ -1,61 +1,156 @@
-import { ActionGetResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { STANDARD } from "../constants";
+import {
+  ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
+  MEMO_PROGRAM_ID,
+} from '@solana/actions';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { STANDARD } from '../constants';
 import puppeteer from 'puppeteer';
-import path from "path";
+import path from 'path';
 import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  Connection,
+  clusterApiUrl,
+  ComputeBudgetProgram,
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from '@solana/web3.js';
 
 const downloadPath = path.resolve(__dirname, 'downloads');
 
 // Ensure the download directory exists
 if (!fs.existsSync(downloadPath)) {
-    fs.mkdirSync(downloadPath);
+  fs.mkdirSync(downloadPath);
 }
 
-async function generateRug(fastify: FastifyInstance) {
-    fastify.route({
-        method: ['GET'],
-        url: '/',
-        schema: {},
-        handler: async (request: FastifyRequest, reply: FastifyReply) => {
-            try {
-                // Launch Puppeteer
-                const browser = await puppeteer.launch({ headless: true });
-                const page = await browser.newPage();
-            
-                // Configure the download behavior
-                await page.goto('https://deanmlittle.github.io/generug/', { waitUntil: 'networkidle2' });
-                const searchResultSelector = 'a#downloadLink';
-                await page.waitForSelector(searchResultSelector);
-                await page.click(searchResultSelector);
-                /*c lient.send('Page.setDownloadBehavior', {
-                  behavior: 'allow',
-                  downloadPath: downloadPath,
-                }); */
-            
-                // Navigate to the website
-                /* await page.goto('https://deanmlittle.github.io/generug/', { waitUntil: 'networkidle2' }); */
-            
+// Function to copy the file using UUID
+const copyFileWithUUID = (sourcePath) => {
+  const id = uuidv4();
+  const newFilename = `${uuidv4()}.png`;
+  const newPath = path.join(path.dirname(sourcePath), newFilename);
+  fs.copyFile(sourcePath, newPath, (err) => {
+    if (err) throw err;
+    console.log(`File copied to ${newFilename}`);
+  });
 
-                /* const payload: ActionGetResponse = {
-                    icon: `${request.protocol}://localhost:5000/public/generug_cover.png`,
-                    label: "Mint your Generug!",
-                    description: "Get Rug with your unique generug",
-                    title: "Mint your Generug",
-                } */
-            /* return reply
-                .code(STANDARD.OK.statusCode)
-                .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
-                .send(payload); */
-            } catch(err) {
-                console.log(err);
-            }
-
-            return reply
-                .code(STANDARD.OK.statusCode)
-                .headers(ACTIONS_CORS_HEADERS as Record<string, string>);
-        }
-    });
+  return id;
 };
+
+async function generateRug(fastify: FastifyInstance) {
+  fastify.route({
+    method: ['GET', 'OPTIONS'],
+    url: '/',
+    schema: {},
+    handler: async (request: FastifyRequest, reply: FastifyReply) => {
+      console.log(request.hostname);
+      const payload: ActionGetResponse = {
+        icon: `${request.protocol}://localhost:5000/public/generug_cover.png`,
+        label: 'Mint your Generug!',
+        description: 'Get Rug with your unique generug',
+        title: 'Mint your Generug',
+      };
+
+      return reply
+        .code(STANDARD.OK.statusCode)
+        .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
+        .send(payload);
+    },
+  });
+
+  fastify.post(
+    '/',
+    {},
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const id = uuidv4();
+
+        console.log(id);
+
+        // Launch Puppeteer
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        const client = await page.target().createCDPSession();
+        await client.send('Page.setDownloadBehavior', {
+          behavior: 'allow',
+          downloadPath: downloadPath,
+        });
+
+        // Configure the download behavior
+        await page.goto('https://deanmlittle.github.io/generug/', {
+          waitUntil: 'networkidle2',
+        });
+
+        console.log('goto');
+        const searchResultSelector = 'a#downloadLink';
+        await page.waitForSelector(searchResultSelector);
+        console.log('waitfor');
+        await page.click(searchResultSelector);
+
+        console.log('click');
+
+        const generugId = await copyFileWithUUID(
+          `${downloadPath}\\generug.png`,
+        );
+
+        console.log(generugId);
+
+        const body = request.body as ActionPostRequest;
+
+        let account: PublicKey;
+        account = new PublicKey(body.account);
+
+        const transaction = new Transaction();
+
+        transaction.add(
+          ComputeBudgetProgram.setComputeUnitPrice({
+            microLamports: 1000,
+          }),
+          new TransactionInstruction({
+            programId: new PublicKey(MEMO_PROGRAM_ID),
+            data: Buffer.from('this is a simple memo message', 'utf8'),
+            keys: [],
+          }),
+        );
+
+        transaction.feePayer = account;
+
+        const connection = new Connection(clusterApiUrl('devnet'));
+        transaction.recentBlockhash = (
+          await connection.getLatestBlockhash()
+        ).blockhash;
+
+        const RPC_ENDPOINT = "https://api.devnet.solana.com";
+        const umi = createUmi(RPC_ENDPOINT);
+
+        const payload: ActionPostResponse = await createPostResponse({
+          fields: {
+            transaction,
+            message: 'Thanks for the coffee',
+          },
+          // signers: []
+        });
+
+        console.log(payload);
+
+        return reply
+          .code(STANDARD.ACCEPTED.statusCode)
+          .headers(ACTIONS_CORS_HEADERS as Record<string, string>)
+          .send(payload);
+      } catch (err) {
+        console.log(err);
+      }
+
+      return reply
+        .code(STANDARD.OK.statusCode)
+        .headers(ACTIONS_CORS_HEADERS as Record<string, string>);
+    },
+  );
+}
 
 export default generateRug;
